3b748e1fadca4ba8561e1fd8540b0eef6e0e2ee7
Kevin Chueh <kevinchueh@ypcloud.com>
2018-11-09 18:01:05 +0800

    trigger -> onEvent

--- flow-event/flow-event.js
+++ flow-event/flow-event.js
@@ -1,46 +1,33 @@
-const events = require('events')
-const flowEvent = new events.EventEmitter()
-
 module.exports = function (RED) {
     "use strict"
 
+    let { setEvent, removeEvent } = require('../lib/mcClient.js')
     function flowOnEvent(config) {
         RED.nodes.createNode(this, config)
         let node = this
         node.name = config.name
 
-        let defaultEventName = config.event || ''
-        if (defaultEventName != '') {
-            flowEvent.on(`${defaultEventName}`, (msg) => {
-                node.send(msg)
-            })
+        let callback = msg =>  node.send({...msg})
+        //let callback = msg => { console.log(msg) }
+        let defaultEventName = config.event
+        if (defaultEventName === 'status') {
+            setEvent('status', callback)
         }
 
+        if (defaultEventName === 'message') {
+            setEvent('message', callback)
+        }
+
+        /*
         node.on('input', msg => {
-            if (!msg.payload) return
-            let { eventName } = msg.payload
             if (eventName == '') return
             flowEvent.once(`${eventName}`, (msg) => {
                 node.send(msg)
             })
         })
-        node.on('close', () => { flowEvent.removeAllListeners() })
+        */
+        node.on('input', node.send)
+        node.on('close', () => { removeEvent(defaultEventName, callback) })
     }
     RED.nodes.registerType("onEvent", flowOnEvent)
-
-    function flowEmit(config) {
-        RED.nodes.createNode(this, config)
-        let node = this
-        node.name = config.name
-
-        let eventName = config.emit || ''
-        node.on('input', msg => {
-            if (!msg.payload) return 
-            eventName = msg.payload.eventName || eventName
-            if (eventName == '') return
-            flowEvent.emit(eventName, msg)
-            node.send({eventName, msg})
-        })
-    }
-    RED.nodes.registerType("emit", flowEmit)
 }
