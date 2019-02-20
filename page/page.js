8f9a6d200ce36314f77a01b54fa193b6ca484778
kevinchueh <kevinchueh@ypcloud.com>
2019-02-20 13:46:53 +0800

    node-red-contrib-motechat@1.0.0 alpha

--- page/page.js
+++ page/page.js
@@ -1,18 +1,23 @@
 const path = require('path')
 module.exports = (RED) => {
-    
+
     function page(config){
-        RED.nodes.createNode(this.config)
+        RED.nodes.createNode(this, config)
+        const node = this
         const { path: jsPath } = config
-        const correctPath = path.isAbsolute(jsPath) ? 
+        const correctPath = path.isAbsolute(jsPath) ?
             jsPath : path.resolve(jsPath)
-        
-        const exec = require(correctPath)
+
         node.on('input', msg => {
-            console.log(correctPath)
-            if (jsPath.length == 0) return
-            if (typeof exec !== 'function') return
-            node.send(exec(msg))
+            try {
+                const exec = require(correctPath)
+                if (typeof exec !== 'function') {
+                    console.log('NOT_FUNCTION')
+                    node.error('NOT_FUNCTION')
+                    return
+                }
+                node.send(exec(msg))
+            } catch(err) { node.error('REQUIRE_FAIL', err) }
         })
     }
     RED.nodes.registerType("page", page)
